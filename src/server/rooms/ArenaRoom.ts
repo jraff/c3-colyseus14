fe794c8dff1f3024d9de4a65e27daf1bc0666ba6
Glitch (held-quartz) <none>
2020-05-07 04:01:36 +0000

    🏚⛺️ Checkpoint ./src/server/rooms/ArenaRoom.ts:10620335/2625 ./package.json:10620335/8

--- src/server/rooms/ArenaRoom.ts
+++ src/server/rooms/ArenaRoom.ts
@@ -2,45 +2,39 @@ import { Room, Client } from "colyseus";
 import { Entity } from "./Entity";
 import { State } from "./State";
 
+interface MouseMessage {
+  x: number;
+  y: number;
+}
+
 export class ArenaRoom extends Room<State> {
 
   onCreate() {
     this.setState(new State());
     this.state.initialize();
+
+    this.onMessage("mouse", (client, message: MouseMessage) => {
+      
+      const entity = this.state.entities[client.sessionId];
+
+      // skip dead players
+      if (!entity) {
+        console.log("DEAD PLAYER ACTING...");
+        return;
+      }
+
+      // change angle
+      const dst = Entity.distance(entity, message as Entity);
+      entity.speed = (dst < 20) ? 0 : Math.min(dst / 15, 4);
+      entity.angle = Math.atan2(entity.y - message.y, entity.x - message.x);
+    });
+
     this.setSimulationInterval(() => this.state.update());
   }
 
   onJoin(client: Client, options: any) {
     console.log(client.sessionId, "JOINED");
     this.state.createPlayer(client.sessionId);
-  }
-
-  onMessage("*", (client: Client, type, message)) {
-  // onMessage(client: Client, message: any) {
-    const entity = this.state.entities[client.sessionId];
-
-    // skip dead players
-    if (!entity) {
-      console.log("DEAD PLAYER ACTING...");
-      return;
-    }
-
-    // const [command, data] = message;
-    const command = type;
-    const data = message;
-
-  // change angle
-    if (command === "mouse") {
-      const dst = Entity.distance(entity, data as Entity);
-      entity.speed = (dst < 20) ? 0 : Math.min(dst / 15, 4);
-      entity.angle = Math.atan2(entity.y - data.y, entity.x - data.x);
-    }
-        
-    if (command === "sendJSON")
-      {
-        console.log("sendJSON")
-        this.broadcast({type: "sendJSON", data: JSON.stringify(data)}, { except: client });
-      }
   }
 
   onLeave(client: Client) {
